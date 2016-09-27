b7e50ef5c6e2b061772b344bf38662578a561edb
tube1 <tube1@DESKTOP-NB7R2GD>
2016-09-27 10:16:08 +0200

    Add project files.

---
+++ AdventureQuest0.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World w = new World();
+            w.PrintWorld();
+            Console.ReadLine();
+        }
+    }
+}
