a2e3c3e16d70015bfb2f137fb346b2b82e0be942
CornMan <tube123@live.dk>
2016-11-15 13:17:40 +0100

    things

--- AdventureQuest0.1/World.cs
+++ AdventureQuest0.1/World.cs
@@ -9,13 +9,53 @@ namespace AdventureQuest0._1
     class World
     {
         Player player = new Player();
-        public void MovePlayer()
+        
+        public void HandleInput()
         {
-            char input = char.Parse(Console.ReadLine());
+            bool isRunning = true;
+            while (isRunning)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower().Equals("exit") || input.ToLower().Equals("e"))
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    HandleMovement();
+                }
+            }
+        }
+        public void HandleMovement()
+        {
+            char input = Console.ReadKey(true).KeyChar;
             switch (input)
             {
                 case 'w':
                     player.CurrentRoom = player.CurrentRoom.north;
+                    break;
+                case 's':
+                    player.CurrentRoom = player.CurrentRoom.south;
+                    break;
+                case 'd':
+                    player.CurrentRoom = player.CurrentRoom.east;
+                    break;
+                case 'a':
+                    player.CurrentRoom = player.CurrentRoom.west;
+                    break;
+                case 'p':
+                    Console.WriteLine("Are you sure you want to use a potion?");
+                    string areyousure = Console.ReadLine();
+                    areyousure 
+                    if (potion == 0)
+                    {
+                        Console.WriteLine("You don't have any potions left");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You gain 60 life");
+                        player.hp = player.hp + 60;
+                    }
                     break;
             }
         }
